3017a4b2e7ff02dc10eddcf63f00a383547bf5f4
Aybor <borisov_aleks@mail.ru>
2022-01-09 16:23:07 +0300

    добавлен скрипт для добавления ингредиентов в базу

---
+++ backend/import_ingredients_csv.py
@@ -0,0 +1,23 @@
+"""
+    Скрипт для импорта ингредиентов из ingredients.csv в БД DJango
+    Для запуска скрипта нужно выполнить команды:
+                                1) manage.py shell
+                                2) exec(open('import_ingredients_csv.py').read())
+"""
+
+import csv
+from tqdm import tqdm
+from api.models import Ingredient
+
+CSV_PATH = './ingredients.csv'
+
+countSuccess = 0
+Ingredient.objects.all().delete()
+
+with open(CSV_PATH, newline='') as csv_file:
+    spamreader = csv.reader(csv_file, delimiter=',', quotechar=',')
+    print('Loading...')
+    for row in tqdm(spamreader):
+        Ingredient.objects.create(pk=countSuccess, name=row[0], measurement_unit=row[1])
+        countSuccess += 1
+    print(f'{countSuccess} записей успешно создано')
