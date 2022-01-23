e1839b569e955e3a5e8b18e2ac5a304592a9b436
Aybor <borisov_aleks@mail.ru>
2022-01-23 15:17:50 +0300

    попытка с postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#обнови секреты
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
